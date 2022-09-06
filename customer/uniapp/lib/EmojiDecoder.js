2ce7687d338ddfa78d6d20fdddd45b988741e7dd
ZhangHui <jiacai@goeasy.io>
2022-09-06 16:13:41 +0800

    测试editorConfig格式化后

--- customer/uniapp/lib/EmojiDecoder.js
+++ customer/uniapp/lib/EmojiDecoder.js
@@ -4,19 +4,19 @@ class EmojiDecoder {
     patterns = [];
     metaChars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
 
-    constructor(url,emojiMap) {
+    constructor(url, emojiMap) {
         this.url = url || '';
         this.emojiMap = emojiMap || {};
         for (let i in this.emojiMap) {
-            if (this.emojiMap.hasOwnProperty(i)){
-                this.patterns.push('('+i.replace(this.metaChars, "\\$&")+')');
+            if (this.emojiMap.hasOwnProperty(i)) {
+                this.patterns.push('(' + i.replace(this.metaChars, "\\$&") + ')');
             }
         }
     }
 
-    decode (text) {
-        return text.replace(new RegExp(this.patterns.join('|'),'g'),  (match) => {
-            return typeof this.emojiMap[match] != 'undefined' ? '<img height="25px" width="25px" src="'+this.url+this.emojiMap[match]+'" />' : match;
+    decode(text) {
+        return text.replace(new RegExp(this.patterns.join('|'), 'g'), (match) => {
+            return typeof this.emojiMap[match] != 'undefined' ? '<img height="25px" width="25px" src="' + this.url + this.emojiMap[match] + '" />' : match;
         });
     }
 }
