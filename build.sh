0ee1c0e48e6686445fede5cbccb040a7e690d917
glnp <guilin@goeasy.io>
2022-10-09 15:46:17 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+if [ "$1" ]; then
+    ACTION=$1
+fi
+
+# 设置信息
+git config user.name "${GIT_USER}"
+git config user.password "${GIT_PASS}"
+git config user.email "${GIT_EMAIL}"
+
+git remote rm origin
+git remote add origin https://${GIT_USER}:${GIT_PASS}@gitee.com/goeasy-io/demo-customer-service.git
+
+# 强制切换分支
+git fetch
+git checkout -f 跑不通_master
+
+# 设置NPM源，提升安装速度
+npm config set registry https://registry.npmmirror.com
+
+if [ "$ACTION" = "b" ]; then
+    # branch 版本
+    cd support/web
+    newVersion=$(npm version prerelease --no-git-tag-version)
+    vwesionDir=${newVersion:1}
+    cd ../../
+fi
+
+if [ "$ACTION" = "r" ]; then
+    # release 版本
+    cd support/web
+    newVersion=$(npm version patch)
+    vwesionDir=${newVersion:1}
+    cd ../../
+fi
+
+# 创建版本目录
+mkdir $vwesionDir
+
+# 构建agent服务
+cd support/web
+npm install
+npm run build
+mv dist ../../$vwesionDir/agent
+
+# 构建customer服务
+cd ../../customer/uniapp
+npm install
+npm run build
+mv dist/build/h5 ../../$vwesionDir/customer
+
+# 复制demo-cs.html文件
+cd ../../
+cp index.html $vwesionDir/index.html
+
+# 克隆仓库
+git clone https://${GIT_USER}:${GIT_PASS}@gitee.com/goeasy-io/show-cs.git
+# 移动版本目录
+mv $vwesionDir show-cs/
+# 删除外部.git
+#rm -rf .git
+#mv .git  .git_old
+
+# 切换仓库
+cd show-cs
+# 设置信息
+git config user.name "${GIT_USER}"
+git config user.password "${GIT_PASS}"
+git config user.email "${GIT_EMAIL}"
+# 标记推送
+git add $vwesionDir
+git commit -m "$vwesionDir is built"
+git push
