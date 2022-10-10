0b5831757bb65611ff6233a64b84169374eb3098
BettyWbb <betty@goeay.io>
2022-10-10 19:24:03 +0800

    1. 打包前修正mainifest version 2. 删除 vue.confg.js 多余的publicpath

---
+++ customer/uniapp/correctManifestVersion.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const config = require('./package.json');
+
+
+const packageJSON = require('./package.json');
+const manifestJSON = require('./src/manifest.json');
+
+let version = packageJSON.version;
+manifestJSON.versionName = version;
+manifestJSON.h5.publicPath = `/show-cs/${version}/customer/`;
+manifestJSON.h5.router.base = manifestJSON.h5.publicPath;
+
+fs.writeFile(`${__dirname}/src/manifest.json`, JSON.stringify(manifestJSON, null, 2), {
+    encoding: "utf-8"
+}, function (err) {
+    if (err) {
+        console.log('写入versionName失败', err);
+    } else {
+        console.log('写入versionName成功');
+    }
+});
+//
+//
+// fs.writeFile(filename, JSON.stringify(releasePkgJson, null, 2), function (err) {
+//     if(err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log('write package.json success');
+//     console.log(filename);
+//     console.log(releasePkgJson);
+// });
+//
+// fs.readFile('src/manifest.json',
+//     function (err, data) {
+//         if (err) {
+//             console.error(err);
+//         } else {
+//             var _data = JSON.parse(data.toString())
+//             const version = config.version;
+//             _data['versionName'] = version;
+//             _data['h5'].publicPath = `/show-cs/${version}/customer/`;
+//             _data['h5'].router.base = `/show-cs/${version}/customer/`;
+//
+//             _data = JSON.stringify(_data);
+//
+//
+//         }
+//     })
